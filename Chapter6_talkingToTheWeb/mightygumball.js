6288724b7da5f8b70ca2fd9df3ca6496b1674218
Lifu Huang <Lifu Huang>
2017-02-20 15:42:22 -0800

    add request.onredaystatechange

--- Chapter6_talkingToTheWeb/mightygumball.js
+++ Chapter6_talkingToTheWeb/mightygumball.js
@@ -1,12 +1,19 @@
 window.onload = function() {
-    var url = "http://gumball.wickedlysmart.com";
+    var url = "http://gumball.wickedlysmart.com/gumball/gumball.html";
     var request = new XMLHttpRequest();
     request.open("GET", url);
+    //if newer browser version
     request.onload = function() {
         if (request.status === 200) {
             updateSales(request.responseText);
         }
     };
+    //if older browser version
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            updateSales(request.responseText);
+        }
+    }
     request.send(null);
 
     function updateSales(responseText) {
