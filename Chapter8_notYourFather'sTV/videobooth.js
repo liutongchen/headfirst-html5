9e12c47b31ec20fb7aeb3ce007002fca73c103c2
Lifu Huang <Lifu Huang>
2017-03-06 18:25:32 -0800

    deal with pushed buttons

--- Chapter8_notYourFather'sTV/videobooth.js
+++ Chapter8_notYourFather'sTV/videobooth.js
@@ -21,18 +21,69 @@ window.onload = function() {
 function handleControl(e) {
     var id = e.target.getAttribute("id");
     if (id == "play") {
+        pushUnpushButtons("play," ["pause"]);
+
+    } else if (id == "pause") {
+        pushUnpushButtons("pause", ["play"]);
         
+    } else if (id == "loop") {
+        if (isButtonPushed("loop")) {
+            pushUnpushButtons("", ["loop"]);
+        } else {
+            pushUnpushButtons("loop", []);
+        }
+    } else if (id == "mute") {
+        if (isButtonPushed("mute")) {
+            pushUnpushButtons("", ["mute"]);
+        } else {
+            pushUnpushButtons("mute", []);
+        }
     }
 }
 
 function setEffect(e) {
-    
+    var id = e.target.getAttribute("id");
+
+    if (id == "normal") {
+        pushUnpushButtons("normal", ["western", "noir", "scifi"]);
+    } else if (id == "western") {
+        pushUnpushButtons("western", ["normal", "noir", "scifi"]);
+    } else if (id == "noir") {
+        pushUnpushButtons("noir", ["western", "normal", "scifi"]);
+    } else if (id == "scifi") {
+        pushUnpushButtons("scifi", ["normal", "western", "noir"]);
+    }
 }
 
 function setVideo(e) {
-
+    var id = e.target.getAttribute("id");
+    if (id == "video1") {
+        pushUnpushButtons("video1", ["video2"]);
+    } else if (id == "video2") {
+        pushUnpushButtons("video2", ["video1"]);
+    }
 }
 
-function pushUnpushButtons() {
+//helper functions
+function pushUnpushButtons(idToPush, idArrayToUnpush) {
+    if (idToPush != "") {
+        var anchor = document.getElementById(idToPush);
+        var theClass = anchor.getAttribute("class");
+        if (theClass.indexOf("selected") < 0) {
+            theClass = theClass + "selected";
+            anchor.setAttribute("class", theClass);
+            var newImage = "url(image/" + idToPush + "pressed.png)";
+            anchor.style.backgroundImage = newImage;
+        }
+    }
 
+    //to be continued....
+    
+}
+
+function isButtonPushed(id) {
+    //get the id and return true if element of that id is selected
+    var anchor = document.getElementById(id);
+    var theClass = anchor.getAttribute("class");
+    return (theClass.indexOf("selected") >= 0);
 }
