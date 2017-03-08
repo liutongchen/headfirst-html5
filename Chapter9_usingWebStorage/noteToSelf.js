beb9a53f70a88b1594dc04c75237242805a4c9e0
Liutong Chen <liutong.clt@gmail.com>
2017-03-07 23:14:57 -0800

    add exercise

--- Chapter9_usingWebStorage/noteToSelf.js
+++ Chapter9_usingWebStorage/noteToSelf.js
@@ -1,4 +1,21 @@
 localStorage.setItem("sticky_0", "Pick up dry cleaning");
 localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
-var sticky = localStorage.getItem("sticky_0");
-alert(sticky);
+window.onload = function() {
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        if (key.substring(0, 6) == "sticky") {
+            var value = localStorage.getItem(key);
+            addStickyToDOM(value);
+        }
+    }
+}
+
+function addStickyToDOM(value) {
+    var stickies = document.getElementById("stickies");
+    var sticky = document.createElement("li");
+    var span = document.createElement("span"); //why add a span element?
+    span.setAttribute("class", "sticky")
+    span.innerHTML = value;
+    sticky.appendChild(span);
+    stickies.appendChild(sticky);
+}
