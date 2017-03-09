73f7268d43c447500c51ec1da6020f7578e0cd27
Liutong Chen <liutong.clt@gmail.com>
2017-03-08 23:05:27 -0800

    add 'add button' function

--- Chapter9_usingWebStorage/noteToSelf.js
+++ Chapter9_usingWebStorage/noteToSelf.js
@@ -1,21 +1,54 @@
-localStorage.setItem("sticky_0", "Pick up dry cleaning");
-localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
 window.onload = function() {
-    for (var i = 0; i < localStorage.length; i++) {
-        var key = localStorage.key(i);
-        if (key.substring(0, 6) == "sticky") {
-            var value = localStorage.getItem(key);
-            addStickyToDOM(value);
-        }
+    //set up the add button
+    var addButton = document.getElementById("add_button");
+    addButton.onclick = createSticky;
+
+    //set up the clear button
+    var clearLSButton = document.getElementById("clear_button");
+    clearLSButton.onclick = function() {
+        localStorage.clear();
+    }
+
+    var stickiesArray = getStickiesArray();
+
+    for (var i = 0; i < stickiesArray.length; i++) {
+        var key = stickiesArray[i];
+        var value = stickiesArray[key];
+        addStickyToDOM(value);
     }
 }
 
 function addStickyToDOM(value) {
     var stickies = document.getElementById("stickies");
     var sticky = document.createElement("li");
-    var span = document.createElement("span"); //why add a span element?
+    var span = document.createElement("span"); //why add a span element? for css purpose?
     span.setAttribute("class", "sticky")
     span.innerHTML = value;
     sticky.appendChild(span);
     stickies.appendChild(sticky);
+}
+
+function createSticky() {
+    var value = document.getElementById("note_text").value;
+    var time = new Date().getTime();
+    var key = "sticky_" + time;
+    var stickiesArray = getStickiesArray();
+
+    localStorage.setItem(key, value); //Why are key/value pairs add to the localStorage again since they are kept in an array?
+    stickiesArray.push(key);
+    localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+
+    addStickyToDOM(value);
+}
+
+function getStickiesArray() {
+    var stickiesArray = localStorage["stickiesArray"];
+    if (!stickiesArray) {
+        stickiesArray = [];
+        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+    } else {
+        stickiesArray = JSON.parse(stickiesArray);
+    }
+
+    return stickiesArray;
 }
