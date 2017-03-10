e280749212e15a252e608a94b625575582213c4b
Liutong Chen <liutong.clt@gmail.com>
2017-03-09 16:05:13 -0800

    finish noteToSelf project

--- Chapter9_usingWebStorage/noteToSelf.js
+++ Chapter9_usingWebStorage/noteToSelf.js
@@ -13,42 +13,77 @@ window.onload = function() {
 
     for (var i = 0; i < stickiesArray.length; i++) {
         var key = stickiesArray[i];
-        var value = stickiesArray[key];
-        addStickyToDOM(value);
+        var stickyObj = JSON.parse(localStorage[key]); //notice here the "stickyObj" needed to be parsed
+        addStickyToDOM(key, stickyObj);
     }
-}
+};
 
-function addStickyToDOM(value) {
+function addStickyToDOM(key, stickyObj) {
     var stickies = document.getElementById("stickies");
     var sticky = document.createElement("li");
-    var span = document.createElement("span"); //why add a span element? for css purpose?
+    var span = document.createElement("span"); 
+
+    sticky.setAttribute("id", key)
     span.setAttribute("class", "sticky")
-    span.innerHTML = value;
+    sticky.style.backgroundColor = stickyObj.color;
+    span.innerHTML = stickyObj.value;
     sticky.appendChild(span);
     stickies.appendChild(sticky);
+    sticky.onclick = deleteSticky;
 }
 
 function createSticky() {
     var value = document.getElementById("note_text").value;
-    var time = new Date().getTime();
-    var key = "sticky_" + time;
+    var currentDate = new Date();
+    var key = "sticky_" + currentDate.getTime();
     var stickiesArray = getStickiesArray();
+    var colorSelectObj = document.getElementById("note_color");
+    var index = colorSelectObj.selectedIndex;
+    var color = colorSelectObj[index].value;
+    var stickyObj = {
+        "value": value,
+        "color": color
+    }
 
-    localStorage.setItem(key, value); //Why are key/value pairs add to the localStorage again since they are kept in an array?
+    localStorage.setItem(key, JSON.stringify(stickyObj));
     stickiesArray.push(key);
     localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
 
-    addStickyToDOM(value);
+    addStickyToDOM(key, stickyObj);
 }
 
 function getStickiesArray() {
-    var stickiesArray = localStorage["stickiesArray"];
+    var stickiesArray = localStorage.getItem("stickiesArray");
+
     if (!stickiesArray) {
         stickiesArray = [];
         localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
     } else {
+        console.log(stickiesArray); //test
         stickiesArray = JSON.parse(stickiesArray);
     }
-
     return stickiesArray;
+}
+
+function deleteSticky(e) {
+    var key = e.target.id;
+    if (e.target.tagName.toLowerCase() == "span") {
+        key = e.tagName.parentNode.id;
+    }
+
+    localStorage.removeItem(key);
+    console.log("remove"); //test
+    var stickiesArray = getStickiesArray();
+    var index = stickiesArray.indexOf(key);
+    console.log("index&key: ", index, " ", key); //test
+    if (index >= 0) {
+        stickiesArray.splice(index, 1);
+    }
+    localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+    removeStickyFromDOM(key)
+}
+
+function removeStickyFromDOM(key) {
+    var sticky = document.getElementById(key);
+    sticky.parentNode.removeChild(sticky);
 }
